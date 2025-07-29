8410bbc7ae522a5044a3578ed09fdd5e4ee73c43
Escape <neeldiyora5@gmail.com>
2025-07-29 19:01:50 +0530

    initialized backend with Spring boot

---
+++ backend_SpringBoot/src/test/java/com/Dev/backend_SpringBoot/BackendSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dev.backend_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
