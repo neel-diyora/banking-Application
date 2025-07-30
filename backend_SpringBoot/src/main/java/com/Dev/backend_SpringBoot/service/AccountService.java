0cd3cf725089549f0898d88b30e495c06eef8d9b
Escape <neeldiyora5@gmail.com>
2025-07-30 09:51:01 +0530

    Account Service

---
+++ backend_SpringBoot/src/main/java/com/Dev/backend_SpringBoot/service/AccountService.java
@@ -0,0 +1,44 @@
+package com.Dev.backend_SpringBoot.service;
+
+import com.Dev.backend_SpringBoot.entity.Account;
+import com.Dev.backend_SpringBoot.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Optional<Account> getAccountByAccountNumber(String accountNumber) {
+        return accountRepository.findByAccountNumber(accountNumber);
+    }
+
+    @Transactional
+    public void transferMoney(String fromAccountNumber, String toAccountNumber, BigDecimal amount) {
+        Account fromAccount = accountRepository.findByAccountNumber(fromAccountNumber)
+                .orElseThrow(() -> new RuntimeException("Sender account not found"));
+        Account toAccount = accountRepository.findByAccountNumber(toAccountNumber)
+                .orElseThrow(() -> new RuntimeException("Receiver account not found"));
+
+        if (fromAccount.getBalance().compareTo(amount) < 0) {
+            throw new RuntimeException("Insufficient funds");
+        }
+
+        fromAccount.setBalance(fromAccount.getBalance().subtract(amount));
+        toAccount.setBalance(toAccount.getBalance().add(amount));
+
+        accountRepository.save(fromAccount);
+        accountRepository.save(toAccount);
+    }
+}
