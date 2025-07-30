44b9c24cb9294fc1a216ba9370b1a9e807c89a57
Escape <neeldiyora5@gmail.com>
2025-07-30 12:34:49 +0530

    HomePage Component

---
+++ frontend_react/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h2>Welcome to the Future of Banking</h2>
+            <p>Use the navigation above to manage your accounts.</p>
+        </div>
+    );
+};
+
+export default HomePage;
