efb72bade48551e80fc603bc64a956fa41cd0623
Escape <neeldiyora5@gmail.com>
2025-07-30 09:33:54 +0530

    Account Repository

---
+++ backend_SpringBoot/src/main/java/com/Dev/backend_SpringBoot/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.Dev.backend_SpringBoot.repository;
+
+import com.Dev.backend_SpringBoot.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByAccountNumber(String accountNumber);
+}
